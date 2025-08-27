75e5e121607474f8055d579d541e104f8f443e7f
Arabboy Ismoilov <arabboyismoilov27@gmail.com>
2025-08-27 09:37:15 +0500

    takrorlovchi handler qoshildi

---
+++ takrorlovchi_handler.py
@@ -0,0 +1,9 @@
+from aiogram import Router, F
+from aiogram.types import Message
+
+router = Router()
+
+
+@router.message(F.text)
+async def handle_text(message: Message):
+    await message.answer(f"siz yozdiz: {message.text}")
